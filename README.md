2dce95eaa2641152a92606862ef4cd821e4c94a3
Franklin Ezeji <franklinezeji@gmail.com>
2020-05-12 17:36:37 +0100

    Add TaxiFarePredictionModel

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# TaxiFarePredictionModel
-This is a machine learning model for predicting taxi fare amount for different vendors in New York City.
+# Taxi Fare Prediction Model
+
+This is a machine learning model developed with ML.NET library owned by Microsoft for predicting taxi fare amount for different taxi vendors in New York City.
+The model was built using the regression task approach with an accuracy value of 0.96 showing the trust-worthiness of the model for predictions.
+
---
+++ TaxiFarePrediction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TaxiFarePredictionML.Model;
+
+namespace TaxiFarePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create sample data
+            ModelInput input = new ModelInput()
+            {
+                Vendor_id = "CMT",
+                Rate_code = 1,
+                Passenger_count = 1,
+                Trip_distance = 1.4f,
+                Payment_type = "CRD"
+            };
+
+            // Make prediction
+            ModelOutput prediction = ConsumeModel.Predict(input);
+
+            // Print Prediction
+            Console.WriteLine($"Predicted Fare: {prediction.Score}");
+            Console.ReadKey();
+        }
+    }
+}
